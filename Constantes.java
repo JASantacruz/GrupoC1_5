2af21f7e7b85ca887d5a3376e744bca23f5e105c
MARodriguezFS <72468134+MARodriguezFS@users.noreply.github.com>
2020-11-07 14:28:49 +0100

    Add files via upload

---
+++ Constantes.java
@@ -0,0 +1,9 @@
+package Practica_Laberinto;
+
+public interface Constantes {
+	public final static int COSTE = 1;
+	public final static char MOV_N = 'N';
+	public final static char MOV_E = 'E';
+	public final static char MOV_S = 'S';
+	public final static char MOV_O = 'O';
+}
